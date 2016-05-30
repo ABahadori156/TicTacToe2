209793830ba1eca6629877a3b08b42bb9c9910d2
Amir Bahadori <pbahadori156@gmail.com>
2016-05-30 13:06:45 -0500

    Added Buttons

--- Tic Tac Toe REDUE/ViewController.m
+++ Tic Tac Toe REDUE/ViewController.m
@@ -9,6 +9,16 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *buttonOne;
+@property (weak, nonatomic) IBOutlet UIButton *buttonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFour;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFive;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSix;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSeven;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEight;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
 @end
 
@@ -19,9 +29,12 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+// When a button is tapped, change the label to X or O based on whose turn it is
+// Ensure X's are blue and O's are red
+// Ensure whichPlayerLabel shows either an X or an O indicating the player who should go next
+-(void)onButtonTapped: (UIButton *)sender {
+    
 }
 
 @end
