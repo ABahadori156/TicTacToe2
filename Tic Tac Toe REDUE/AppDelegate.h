21da9e8e74b022e7275c639f9044a6bb3a3c613d
Amir Bahadori <pbahadori156@gmail.com>
2016-05-30 12:38:10 -0500

    Initial Commit

---
+++ Tic Tac Toe REDUE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tic Tac Toe REDUE
+//
+//  Created by Pasha Bahadori on 5/30/16.
+//  Copyright © 2016 Pelican Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
